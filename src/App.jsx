da3049af92256c9975c47d94e7853c8823bd82e9
Jasaerys Bojan <gm.jasbojan@gmail.com>
2025-03-01 22:44:03 +0530

    Implementing TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 
 import './App.css'
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import Search from './components/Search'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  header: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();  
+    }, []);
+  
 
   return (
     <main>
@@ -18,9 +44,14 @@ const App = () => {
           <h1>
             Find <span className="text-gradient">Trending</span> Movies You'll Enjoy
           </h1>
-        </header>
-        
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className='all-movies'>
+          <h2> All Movies</h2>
+
+          {errorMessage && <p className='text-red-500'>{errorMessage} </p>}
+        </section>
+
       </div>
 
     </main>
